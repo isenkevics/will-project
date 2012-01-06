8faa655d02993b90ef1f2f26c7253c220bbe8d17
ilmars <ilmarssenk@gmail.com>
2012-01-06 01:05:42 -0200

    Inicial

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿//my first C# program
+
+// Hello Word
+//using System;
+
+using System.Security;
+using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
+
+
+static class Hello1 
+{ 
+    //[STAThread] 
+    public static void Main(string[] args)
+    {
+        bool consoleMode = Boolean.Parse(args[0]); //true; // ;
+ 
+        if (consoleMode) { 
+            System.Console.WriteLine("consolemode started");
+            System.Diagnostics.Debug.WriteLine("consolemode started");
+        } else {
+            System.Diagnostics.Debug.WriteLine("Hello, World!");
+            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Hello, World!");
+            System.Diagnostics.Debug.WriteLine("Hello, World!");
+        }
+        System.Console.WriteLine("consolemode finished");
+        System.Diagnostics.Debug.WriteLine("consolemode finished");
+    }
+}
+
+
+
