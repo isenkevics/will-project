d6c8519c88ae2f0a19b6e6a9ddfe6149fe9bdd48
ilmars <ilmarssenk@gmail.com>
2012-01-07 01:50:30 -0200

    Inserção de mais uma linha de mensagem!

--- JAVA/HelloWorldApp/src/helloworldapp/HelloWorldApp.java
+++ JAVA/HelloWorldApp/src/helloworldapp/HelloWorldApp.java
@@ -17,5 +17,6 @@ public class HelloWorldApp {
        System.out.println("Hello World, MOÇADA!");
        System.out.println("Hello World, MOÇADA!");
        System.out.println("Hello World, MOÇADA!");
+       System.out.println("Hello World, MOÇADA!");
     }
 }
